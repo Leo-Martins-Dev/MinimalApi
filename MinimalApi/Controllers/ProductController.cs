fdce835ad8374701e5a91bee585be9067ce39e47
Leo-Martins-Dev <leonardomartins578@gmail.com>
2023-10-08 12:49:25 -0300

    FirstCommit

---
+++ MinimalApi/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using MinimalApi.Services;
+
+namespace MinimalApi.Controllers
+{
+    public class ProductController : Controller
+    {
+
+
+    }
+
+}
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,9 +1,13 @@
+using MinimalApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<ProductServiceMock>();
 
 var app = builder.Build();
 
@@ -16,4 +20,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/products", (ProductServiceMock productService) => productService.GetProducts());
+app.MapGet("/products/{id}", (ProductServiceMock productService, int id) => productService.GetProduct(id));
 
+app.Run();
